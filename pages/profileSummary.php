db4dcb60d59a1bd0458c2580809d8dec0e31c04d
Mason Hernandez <mhernandez58@mail.greenriver.edu>
2018-01-27 20:05:16 -0500

    Setup directories as needed

---
+++ pages/profileSummary.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mason
+ * Date: 1/27/2018
+ * Time: 5:02 PM
+ */
---
+++ pages/setup/profileSetup.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * User: Mason Hernandez
+ * Date: 1/27/2018
+ * Time: 4:56 PM
+ */
