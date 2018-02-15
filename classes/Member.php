62dc8b945a5ad045fe205bb338c8d6deb185d200
Mason Hernandez <mhernandez58@mail.greenriver.edu>
2018-02-15 16:24:04 -0500

    Created Member class and PeremiumMember class

---
+++ classes/Member.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mason Hernandez
+ * Date: 2/14/2018
+ * Time: 5:44 PM
+ */
+
+class Member
+{
+    private $fname = "";
+    private $lname = "";
+    private $age = "";
+    private $gender = "";
+    private $phone = "";
+    private $email = "";
+    private $state = "";
+    private $seeking = "";
+    private $bio = "";
+
+    function __construct($fname, $lname, $age, $gender, $phone)
+    {
+        $this->fname = $fname;
+        $this->lname = $lname;
+        $this->age = $age;
+        $this->gender = $gender;
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFname()
+    {
+        return $this->fname;
+    }
+
+    /**
+     * @param string $fname
+     */
+    public function setFname($fname)
+    {
+        $this->fname = $fname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLname()
+    {
+        return $this->lname;
+    }
+
+    /**
+     * @param string $lname
+     */
+    public function setLname($lname)
+    {
+        $this->lname = $lname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param string $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param string $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param string $state
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSeeking()
+    {
+        return $this->seeking;
+    }
+
+    /**
+     * @param string $seeking
+     */
+    public function setSeeking($seeking)
+    {
+        $this->seeking = $seeking;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBio()
+    {
+        return $this->bio;
+    }
+
+    /**
+     * @param string $bio
+     */
+    public function setBio($bio)
+    {
+        $this->bio = $bio;
+    }
+}
---
+++ classes/PremiumMember.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mason
+ * Date: 2/15/2018
+ * Time: 1:10 PM
+ */
+
+class PremiumMember extends Member
+{
+    private $_outdoor_interests = array('hiking', 'biking', 'swimming', 'collecting', 'walking', 'climbing');
+    private $_indoor_ops = array('tv', 'movies', 'cooking', 'puzzles', 'reading', 'playing cards', 'board games', 'video games');
+}
